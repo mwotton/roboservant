a811a985302baf711dc76ec8b33634875174141d
Mark Wotton <mwotton@gmail.com>
2020-09-26 15:26:50 -0400

    seeded example

---
+++ test/Seeded.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Seeded where
+
+import Data.Aeson
+import Data.Typeable (Typeable)
+import GHC.Generics (Generic)
+import Servant
+
+newtype Seed = Seed Int
+  deriving (Generic, Eq, Show, Typeable)
+  deriving newtype (FromHttpApiData, ToHttpApiData)
+
+instance ToJSON Seed
+instance FromJSON Seed
+
+type Api = Capture "seed" Seed :> Get '[JSON] ()
+      :<|> Get '[JSON] ()
+
+server :: Server Api
+server = (\(Seed _) -> error "we blow up if we get here")
+         :<|> pure ()
