d54960dcecad4c15da63153c4e33731b425c1021
Justin Le <justin@jle.im>
2020-11-17 14:14:09 -0800

    type-safe servant interface

--- src/Roboservant/Direct.hs
+++ src/Roboservant/Direct.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
@@ -22,18 +23,26 @@ module Roboservant.Direct
 where
 
 import Control.Exception.Lifted(throw,Handler(..), Exception,SomeException,SomeAsyncException, catch, catches)
-import Control.Monad(void,replicateM)
+import Control.Monad(void,replicateM,forM_)
 import Control.Monad.State.Strict(MonadState,MonadIO,get,modify',liftIO,execStateT)
 import Control.Monad.Trans.Control(MonadBaseControl)
-import Data.Dynamic (Dynamic, dynApply, dynTypeRep, fromDynamic)
+import Data.Dependent.Map (DMap)
+import Data.Dynamic (Dynamic(..), dynApply, dynTypeRep, fromDynamic)
+import Data.List.NonEmpty (NonEmpty(..))
 import Data.Map.Strict(Map)
 import Data.Maybe (mapMaybe)
 import Data.Typeable (TypeRep)
 import Servant (Endpoints, Proxy (Proxy), Server, ServerError(..))
 import System.Random(StdGen,randomR,mkStdGen)
 import System.Timeout.Lifted(timeout)
+import GHC.Generics ((:*:)(..))
+import qualified Data.Vinyl.Functor as V
+import qualified Data.Vinyl.Curry as V
+import qualified Data.Dependent.Map as DM
+import qualified Data.Vinyl as V
 import qualified Data.List.NonEmpty as NEL
 import qualified Data.Map.Strict as Map
+import qualified Type.Reflection as R
 
 import Roboservant.Types.Breakdown
 import Roboservant.Types
@@ -41,6 +50,10 @@ import Roboservant.Types
     FlattenServer (..),
 --    ReifiedApi,
     ToReifiedApi (..),
+    ReifiedEndpoint(..),
+    Argument(..),
+    TypedF
+
   )
 
 data RoboservantException
@@ -72,7 +85,12 @@ data FuzzState = FuzzState
   , stash :: Stash
   , currentRng :: StdGen
   }
-  deriving (Show)
+  deriving Show
+
+data EndpointOption = forall as. (V.RecordToList as, V.RMap as) => EndpointOption
+    { eoCall :: V.Curried as (IO (Either ServerError (NonEmpty Dynamic)))
+    , eoArgs :: V.Rec (TypedF StashValue) as
+    }
 
 fuzz :: forall api. (FlattenServer api, ToReifiedApi (Endpoints api))
      => Server api
@@ -87,8 +105,11 @@ fuzz server Config{..} checker = do
   -- and throw an exception that propagates through this.
 
   void $ timeout (maxRuntime * 1000000) ( execStateT (replicateM maxReps go) FuzzState{..})
-  mapM_ (print . (\(offset, (args, dyn) ) -> (offset, map fst args, dyn))) reifiedApi
-
+  forM_ reifiedApi $ \(offset, ReifiedEndpoint{..}) ->
+    print (
+        offset
+      , recordToList' (\(tr :*: _) -> R.SomeTypeRep tr) reArguments
+      )
   where
 
     reifiedApi = toReifiedApi (flattenServer @api server) (Proxy @(Endpoints api))
@@ -102,67 +123,77 @@ fuzz server Config{..} checker = do
       modify' $ \st' -> st' { currentRng = newGen }
       pure (l !! index)
 
-    genOp :: (MonadState FuzzState m, MonadIO m)
-          => m (FuzzOp, Dynamic, [Dynamic])
-    genOp = do -- fs@FuzzState{..} = do
+    withOp :: (MonadState FuzzState m, MonadIO m)
+          => (forall as. (V.RecordToList as, V.RMap as)
+                  => FuzzOp
+                  -> V.Curried as (IO (Either ServerError (NonEmpty Dynamic)))
+                  -> V.Rec (TypedF V.Identity) as -> m r
+             )
+          -> m r
+    withOp callback = do
       -- choose a call to make, from the endpoints with fillable arguments.
-      (offset, dynCall, args) <- elementOrFail . options =<< get
-      r <- mapM (elementOrFail . zip [0..] . NEL.toList) args
-      let pathSegment = FuzzOp offset (map (\(index,(_,dyn) ) -> Provenance (dynTypeRep dyn) index) r)
+      (offset, EndpointOption{..}) <- elementOrFail . options =<< get
+      r <- V.rtraverse 
+        (\(tr :*: StashValue svs) -> elementOrFail $
+            zipWith (\i xy -> V.Const i :*: tr :*: xy)
+               [0..]
+               (NEL.toList svs)
+        )
+        eoArgs
+      let pathSegment = FuzzOp offset $
+            recordToList'
+              (\(V.Const index :*: tr :*: _) -> Provenance (R.SomeTypeRep tr) index)
+              r
+          argValues = V.rmap
+            (\(_ :*: tr :*: (_, x)) -> tr :*: V.Identity x)
+            r
       modify' (\f -> f { path = path f <> [pathSegment] })
-      pure (pathSegment, dynCall, fmap (snd . snd) r)
-
+      callback pathSegment eoCall argValues
       where
-        options :: FuzzState -> [(ApiOffset, Dynamic, [NEL.NonEmpty ([Provenance], Dynamic)])]
+        options :: FuzzState -> [(ApiOffset, EndpointOption)]
         options FuzzState{..} =
           mapMaybe
-            ( \(offset, (argreps, dynCall)) -> (offset,dynCall,) <$> do
-                mapM (\(_tr,bf) -> bf stash ) argreps
+            ( \(offset, ReifiedEndpoint{..}) -> do
+              args <- V.rtraverse (\(tr :*: Argument bf) -> (tr :*:) <$> bf stash) reArguments
+              pure (offset, EndpointOption reEndpointFunc args)
             )
             reifiedApi
 
-    execute :: (MonadState FuzzState m, MonadIO m)
-          => (FuzzOp,Dynamic,[Dynamic])  -> m ()
-    execute (fuzzop, dyncall, args) = do
+    execute :: (MonadState FuzzState m, MonadIO m, V.RecordToList as, V.RMap as)
+          => FuzzOp
+          -> V.Curried as (IO (Either ServerError (NonEmpty Dynamic)))
+          -> V.Rec (TypedF V.Identity) as
+          -> m ()
+    execute fuzzop func args = do
       liftIO $ print fuzzop
-      -- now, magic happens: we apply some dynamic arguments to a dynamic
-      -- function and hopefully something useful pops out the end.
-      let func = foldr (\arg curr -> flip dynApply arg =<< curr) (Just dyncall) (reverse args)
       st <- get
-      let showable = unlines $ ("args":map (show . dynTypeRep) args)
+      let showable = unlines $ ("args":map show argTypes)
             <> ["fuzzop"
                , show fuzzop
-               ,"dyncall"
-               ,show (dynTypeRep dyncall)
+               -- ,"dyncall"
+               -- ,show (dynTypeRep dyncall)
                ,"state"
                ,show st]
       liftIO $ putStrLn showable
-      
-      case func of
-        Nothing -> error ("all screwed up 1: " <> maybe ("nothing: " <> show showable) (show . dynTypeRep) func)
-        Just (f') -> do
-          -- liftIO $ print
-          case fromDynamic f' of
-            Nothing -> error ("all screwed up 2: " <> maybe ("nothing: " <> show showable) (show . dynTypeRep) func)
-            Just (f) -> liftIO f >>= \case
-              -- parameterise this
-              Left (serverError :: ServerError) ->
-                case errHTTPCode serverError of
-                  500 -> throw serverError
-                  _ -> do
-                    liftIO $ print ("ignoring non-500 error" , serverError)
-
-              Right (dyn :: NEL.NonEmpty Dynamic) -> do
-                liftIO $ print ("storing", fmap dynTypeRep dyn)
-                modify' (\fs@FuzzState{..} ->
-                  fs { stash = addToStash (NEL.toList dyn) stash } )
-      pure ()
+      liftIO (V.runcurry' func argVals) >>= \case
+        -- parameterise this
+        Left (serverError :: ServerError) ->
+          case errHTTPCode serverError of
+            500 -> throw serverError
+            _ -> do
+              liftIO $ print ("ignoring non-500 error" , serverError)
+        Right (dyn :: NEL.NonEmpty Dynamic) -> do
+          liftIO $ print ("storing", fmap dynTypeRep dyn)
+          modify' (\fs@FuzzState{..} ->
+            fs { stash = addToStash (NEL.toList dyn) stash } )
+      where
+        argVals = V.rmap (\(_ :*: V.Identity x) -> V.Identity x) args
+        argTypes = recordToList' (\(tr :*: _) -> R.SomeTypeRep tr) args
 
     go :: (MonadState FuzzState m, MonadIO m, MonadBaseControl IO m)
          => m ()
-    go = do
-      op <- genOp
-      catches (execute op)
+    go = withOp $ \op func args -> do
+      catches (execute op func args)
         [ Handler (\(e :: SomeAsyncException) -> throw e)
         , Handler (\(e :: SomeException) -> throw . RoboservantException ServerCrashed (Just e)  =<< get)
         ]
@@ -177,17 +208,30 @@ fuzz server Config{..} checker = do
   --       (fmap preload seed <> [callEndpoint reifiedApi])
       --  executeSequential emptyState actions
 addToStash :: [Dynamic]
-           -> Map TypeRep (NEL.NonEmpty ([Provenance], Dynamic))
-           -> Map TypeRep (NEL.NonEmpty ([Provenance], Dynamic))
+           -> Stash
+           -> Stash
 addToStash result stash =
-  foldr (\dyn dict -> let tr = dynTypeRep dyn in
-                        Map.insertWith renumber tr (pure ([Provenance tr 0],dyn)) dict) stash result
+  foldr (\(Dynamic tr x) (Stash dict) -> Stash $
+      DM.insertWith renumber tr (StashValue (([Provenance (R.SomeTypeRep tr) 0],x):|[])) dict
+      -- in  DM.insertWith renumber _ (pure ([Provenance tr 0],x)) dict
+    )
+    stash
+    result
 -- Map.insertWith (flip (<>)) (dynTypeRep result) (_pure result) stash   })
   where
-    renumber :: NEL.NonEmpty ([Provenance],Dynamic)
-             -> NEL.NonEmpty ([Provenance],Dynamic)
-             -> NEL.NonEmpty ([Provenance],Dynamic)
-    renumber singleDyn l = case NEL.toList singleDyn of
+    renumber :: StashValue a
+             -> StashValue a
+             -> StashValue a
+    renumber (StashValue singleDyn) (StashValue l) = StashValue $ case NEL.toList singleDyn of
       [([Provenance tr _], dyn)] -> l
         <> pure ([Provenance tr (length (NEL.last l) + 1)], dyn)
       _ -> error "should be impossible"
+
+
+-- why isn't this in vinyl?
+recordToList'
+    :: (V.RecordToList as, V.RMap as)
+    => (forall x. f x -> a)
+    -> V.Rec f as
+    -> [a]
+recordToList' f = V.recordToList . V.rmap (V.Const . f)
