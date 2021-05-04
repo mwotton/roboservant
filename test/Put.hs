5d71aaaae4e2d984894491616d5c07585b577608
Mark Wotton <mwotton@gmail.com>
2021-05-04 14:13:21 -0400

    include PUT

---
+++ test/Put.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Put where
+
+import Data.Aeson
+import Data.Hashable
+import GHC.Generics (Generic)
+import Servant
+
+type Api =
+  Get '[JSON] Foo
+    :<|> ReqBody '[JSON] Foo :> Put '[JSON] ()
+    :<|> ReqBody '[JSON] Foo :> Put '[JSON] NoContent
+
+data Foo = Foo
+  deriving (Eq, Show, Generic)
+
+instance Hashable Foo
+
+instance ToJSON Foo
+
+instance FromJSON Foo
+
+server :: Server Api
+server =
+  pure Foo
+    :<|> const (pure ())
+    :<|> const (pure NoContent)
